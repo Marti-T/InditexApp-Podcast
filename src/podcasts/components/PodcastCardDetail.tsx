798d2c5ef8c84188cf85e14e885f073f3d49b1e3
Martí Touriño <marti.tourino@patterson.agency>
2024-03-17 15:13:49 +0100

    Modify PodcastCard

--- src/podcasts/components/PodcastCardDetail.tsx
+++ src/podcasts/components/PodcastCardDetail.tsx
@@ -8,9 +8,12 @@ export const PodcastCardDetail: FC = () => {
 
   const { chapterId } = useParams<{ chapterId: string }>();
   const [ chapter, setPodcastChapter ] = useState<PodcastDetail | null>(null);
+  const [ isLoading, setIsLoading ] = useState<boolean>(true);
   const [ errorMessage, setErrorMessage ] = useState<string>('');
 
   useEffect(() => {
+
+    setIsLoading(true);
 
     const localStorageCollectionsList = localStorage.getItem('localStorageCollectionsList');
 
@@ -27,13 +30,21 @@ export const PodcastCardDetail: FC = () => {
       setErrorMessage(`The chapter ${ chapter?.trackName } has not been found`);
     }
 
+    setIsLoading(false);
+
   }, [chapterId]);
 
 
   return (
     <div className="podcast-card-detail">
       <div className="podcast-card-detail__content">
-        { errorMessage ? (
+        { isLoading ? (
+          <div className="loader">
+            <div className="loader__content">
+              <div className="loader__spinner"></div>
+            </div>
+          </div>
+        ) : errorMessage ? (
           <div className="error-message">
             <div className="error-message__content">
               { errorMessage }
